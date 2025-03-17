1f364c6cc56627570518c78d7cb0a46a89e3657c
Jaakko Lipas <jaakko.lipas@gmail.com>
2025-03-17 12:12:43 +0200

    Lecture 8 commit

--- GProg-Demo/Assets/AG3958/Scripts/BoosterPad.cs
+++ GProg-Demo/Assets/AG3958/Scripts/BoosterPad.cs
@@ -4,22 +4,20 @@ using UnityEngine;
 
 namespace AG3958
 {
-    public class BoosterPad : TrackObject
+    public sealed class BoosterPad : TrackObject
     {
-        public override string TypeOfObject { get; set; }
-        public override List<string> CollisionTagList { get; set; }
-
         [SerializeField] private float boostMagnitude;
         [SerializeField] private float boostTime;
         private bool boostOn;
         private float originalSpeed;
         private PatrolPather patroller;
+        private Rigidbody rb;
 
         private void Start()
         {
-            TypeOfObject = "Booster";
-            CollisionTagList = new List<string>();
-            CollisionTagList.Add("Vehicle");
+            this.TypeOfObject = "Booster";
+            this.CollisionTagList = new List<string>();
+            this.CollisionTagList.Add("Vehicle");
             boostOn = false;
         }
 
@@ -32,11 +30,22 @@ namespace AG3958
             }
         }
 
-        IEnumerator BoostTime()
+        private IEnumerator BoostTime()
         {
             yield return new WaitForSeconds(boostTime);
-            patroller.speed = originalSpeed;
             boostOn = false;
+            StartCoroutine(DecelGradual());
+        }
+
+        private IEnumerator DecelGradual()
+        {
+            while (patroller.speed > originalSpeed * 1.01f)
+            {
+                if (boostOn) break;
+                patroller.speed = Mathf.Lerp(patroller.speed, originalSpeed, 0.02f);
+                yield return new WaitForSeconds(0.1f);
+            }
+            patroller.speed = originalSpeed;
         }
 
         public override void OnTriggerEnter(Collider other)
@@ -45,10 +54,10 @@ namespace AG3958
             {
                 if (other.gameObject.CompareTag(tag))
                 {
-                    if (other.gameObject.TryGetComponent<PatrolPather>(out PatrolPather pp))
+                    if (other.TryGetComponent<PatrolPather>(out PatrolPather pp))
                     {
-                        originalSpeed = pp.speed;
                         patroller = pp;
+                        originalSpeed = patroller.speed;
                         boostOn = true;
                     }
                 }
--- GProg-Demo/Assets/AG3958/Scripts/TrackObject.cs
+++ GProg-Demo/Assets/AG3958/Scripts/TrackObject.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class TrackObject : MonoBehaviour
+namespace AG3958
 {
-    public abstract string TypeOfObject { get; set; }
-    public abstract List<string> CollisionTagList { get; set; }
+    public abstract class TrackObject : MonoBehaviour
+    {
+        internal string TypeOfObject { get; set; }
+        internal List<string> CollisionTagList { get; set; }
 
-    public abstract void OnTriggerEnter(Collider other);
+        public abstract void OnTriggerEnter(Collider other);
+    } 
 }
